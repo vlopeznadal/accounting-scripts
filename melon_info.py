1c0bff5529c54ec43efbaeaa59702db660ae59e8
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-04 19:21:11 -0500

    Completed melons.py and melon_info.py

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,16 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
 
 
-def print_melon(name, seedless, price):
+def print_melon(melons):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon, characteristic in melons.items():
+        print(melon.upper())
+        for characteristic, value in characteristic.items():
+            print(f"{characteristic} : {value}")
+        print(" ")
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_melon(melons)
--- melons.py
+++ melons.py
@@ -1,23 +1,41 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+melons = {
+    'Honeydew': {
+        'price': 0.99,
+        'seedless': True,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+    },
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+    'Crenshaw': {
+        'price': 2.00,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+    },
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+    'Crane': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+    },
+
+    'Casaba': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+    },
+
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'weight': None,
+    }
 }
